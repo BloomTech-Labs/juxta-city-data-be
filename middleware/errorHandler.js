a1f13c7aef522d8d4fef9a4cbfb2ce7a11915803
alfonsoruiz <1800ruiz@gmail.com>
2020-02-24 20:03:26 -0800

    Setup middlware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,6 @@
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ success: false, message: err });
+};
+
+module.exports = errorHandler;
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const errorHandler = require('./middleware/errorHandler');
+
 const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(morgan('dev'));
 
 server.get('/', (req, res) => {
   res.send('Hello City');
+});
+
+server.use(errorHandler);
+
+// 404 Handler
+server.use((req, res, next) => {
+  res.status(404).json({ success: false, message: 'Not found' });
 });
 
 module.exports = server;
