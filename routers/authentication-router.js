01c2e612fb0aa312c597d6b3be3f623ec55cd500
Zac Smith <zrs3141592@gmail.com>
2020-03-18 11:35:58 -0600

    basic passport set up
    Use Github, Twitter, Facebook, Google or LinkedIn for authenticaiton. Next will be to add local authenticaion.

---
+++ routers/authentication-router.js
@@ -0,0 +1,21 @@
+const router = express('express').Router()
+const passport = require('passport')
+
+
+// Github authentication
+
+
+// Google authentication
+
+
+// Twitter authentication
+
+
+// Facebook authentication
+
+
+// LinkedIn authentication
+
+
+
+module.exports = router;
