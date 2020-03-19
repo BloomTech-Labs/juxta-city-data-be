1913d2295e09ef07b95e1722b27bfcb749374041
Zac Smith <zrs3141592@gmail.com>
2020-03-18 20:00:38 -0600

    note changes for new migrations

--- data/migrations/20200219203432_users.js
+++ data/migrations/20200219203432_users.js
@@ -2,16 +2,21 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', user=>{
       user.increments();
-      user.string('username',124).notNullable().unique();
-      user.string('email', 124).notNullable().unique();
-      user.string('password', 248).notNullable();
-      user.string('first_name', 124).notNullable();
-      user.string('last_name', 124).notNullable();
-      user.date('dob').notNullable();
+      user.string('username',124).notNullable().unique(); // Leave
+      user.string('email', 124).notNullable().unique();  // Remove .notNullable()
+      user.string('password', 248).notNullable();   // Leave
+      user.string('first_name', 124).notNullable(); // Remove .notNullable()
+      user.string('last_name', 124).notNullable(); // Remove .notNullable()
+      user.date('dob').notNullable();  // Remove .notNullable()
       user.string('address', 248);
       user.string('city', 124);
       user.string('state', 124);
       user.integer('zip', 62);
+      // googleId
+      // facebookId
+      // githubId
+      // twitterId
+      // linkedId
   })
   .createTable('favorites', favorite=>{
       favorite.integer('user_id').unsigned().notNullable().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
