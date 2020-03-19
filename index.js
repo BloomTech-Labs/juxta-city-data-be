05cc9cee743bc510ab760d6d461f094c244b0ec4
Zac Smith <zrs3141592@gmail.com>
2020-03-18 20:01:09 -0600

    port change, look at dotenv

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./server');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7500;
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
