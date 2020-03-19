1e4517cd550f15596d89aa8ffbe5a7b81810a458
Zac Smith <zrs3141592@gmail.com>
2020-03-18 23:04:00 -0600

    update files to include .env

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const dbConnection = process.env.DATABASE_URL;
 
+
 module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host: '127.0.0.1',
-      database: 'findur-db',
-      user: 'postgres',
-      password: process.env.PASSWORD,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
       directory: './data/migrations',
