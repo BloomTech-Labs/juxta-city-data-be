5d23884c48cc1e4592e78be730407362276cb49c
Zac Smith <zrs3141592@gmail.com>
2020-03-20 17:58:17 -0600

    update auth route

---
+++ routers/authentication-router.js
@@ -0,0 +1,25 @@
+const router = express('express').Router()
+const passport = require('passport')
+
+
+// Github authentication
+router.get('/auth/github',
+  passport.authenticate('github', { scope: ['profile'] }));
+
+router.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+// Google authentication
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
