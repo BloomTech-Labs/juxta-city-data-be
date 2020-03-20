01638a127fc7f546f2ac07644acd8868db42449b
Zac Smith <zrs3141592@gmail.com>
2020-03-20 17:45:37 -0600

    conditional for dotenv

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-require('dotenv').config()
+if (process.env.ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const server = require("./api/server.js");
 
 const PORT = process.env.PORT || 6000;
 
-server.listen(PORT, function() {
+server.listen(PORT, function () {
   console.log(`\n *** Service is running on localhost:${PORT} *** \n`);
 });
