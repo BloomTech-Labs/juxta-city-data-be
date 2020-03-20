8ac15834291d4a428ce1e3b337451eda28adfaa6
Zac Smith <zrs3141592@gmail.com>
2020-03-20 13:57:54 -0600

    update index

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const server = require('./server');
+const server = require("./api/server.js");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
-server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+server.listen(PORT, function() {
+  console.log(`\n *** Service is running on localhost:${PORT} *** \n`);
 });
