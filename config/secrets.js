5437b2ba6793e6e2ea81054a9eee5d600173e4a0
Zac Smith <zrs3141592@gmail.com>
2020-03-20 17:54:47 -0600

    update strategies

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,14 @@
 module.exports = {
-  jwtSecret:
-    process.env.JWT_SECRET || "the secret message is drink more ovaltine"
-};
+  google: {
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET
+  },
+  github: {
+    clientID: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET
+  },
+  session: {
+    cookieKey: process.env.SESSION_COOKIE_KEY
+  },
+  jwtSecret: process.env.JWT_SECRET || "the secret message is drink more ovaltine"
+}
