db6b2e41fce3bef5ce4cec1617b1db4a356171d1
Zac Smith <zrs3141592@gmail.com>
2020-03-20 14:01:56 -0600

    update config

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
 const config = require('../knexfile');
-const environment = process.env.DB_ENV || 'development';
+const dbENV = process.env.DB_ENV || 'development';
 
-module.exports = knex(config[environment]);
+module.exports = knex(config[dbENV]);
