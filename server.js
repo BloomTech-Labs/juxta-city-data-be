e0edaea663ce8576ae88cc1fc7b0b856b406b1ae
jackskim <jackskim29@gmail.com>
2020-04-25 06:19:28 -0700

    Remove extra error handler

--- server.js
+++ server.js
@@ -1,34 +1,28 @@
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const errorHandler = require('./middleware/errorHandler');
-const usersRouter = require('./routers/users-router.js');
-const { oidcSession, oidc } = require('./middleware/oidc-router.js')
+const errorHandler = require("./middleware/errorHandler");
+const usersRouter = require("./routers/users-router.js");
+//const { oidcSession, oidc } = require('./middleware/oidc-router.js')
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-server.use(morgan('dev'));
-server.use(oidcSession)
-server.use(oidc.router)
+server.use(morgan("dev"));
+server.use(oidcSession);
+server.use(oidc.router);
 
-
-server.get('/', (req, res) => {
-  res.send('Hello City');
+server.get("/", (req, res) => {
+  res.send("Hello City");
 });
 
-server.use('/api/users', usersRouter);
+server.use("/api/users", usersRouter);
 
 server.use(errorHandler);
-
-// 404 Handler
-server.use((req, res, next) => {
-  res.status(404).json({ success: false, message: 'Not found' });
-});
 
 module.exports = server;
