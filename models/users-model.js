55ae50be93105b360670a59b9309e048012f3501
jackskim <jackskim29@gmail.com>
2020-04-27 13:43:39 -0700

    Prettier auto-format
    Co-authored-by Zac Smith <zrs31411592@gmail.com>

--- models/users-model.js
+++ models/users-model.js
@@ -1,42 +1,31 @@
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
 
 async function add(user) {
-  const [ id ] = await db('users').returning('id').insert(user);
+  const [id] = await db("users").returning("id").insert(user);
 
-  return db('users')
-    .where({ id })
-    .first();
+  return db("users").where({ id }).first();
 }
 
 function getBy(filter) {
-  return db('users')
-    .where(filter)
+  return db("users").where(filter);
 }
 
 function getById(id) {
-  return db('users')
-    .where({ id })
-    .first();
+  return db("users").where({ id }).first();
 }
 
 function getAll() {
-  return db('users');
+  return db("users");
 }
 
 function remove(id) {
-  return db('users')
-    .where({ id })
-    .del();
+  return db("users").where({ id }).del();
 }
 
 async function update(user, id) {
-  await db('users')
-    .where({ id })
-    .update(user);
+  await db("users").where({ id }).update(user);
 
-  return db('users')
-    .where({ id })
-    .first();
+  return db("users").where({ id }).first();
 }
 
 module.exports = {
@@ -45,5 +34,5 @@ module.exports = {
   getById,
   getAll,
   remove,
-  update
-}
+  update,
+};
