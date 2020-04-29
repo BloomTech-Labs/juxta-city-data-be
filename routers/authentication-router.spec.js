8df97d30ea069404dc4f21ba8991873ab8513580
Zac Smith <zrs3141592@gmail.com>
2020-04-28 18:12:10 -0600

    update path

---
+++ routers/authentication-router.spec.js
@@ -0,0 +1,14 @@
+// const request = require("supertest");
+
+// const server = require("../server.js");
+
+// describe("authentication-router.js", () => {
+//   it("", function (done) {
+//     request
+//       .post("/")
+//       .send({
+//         username: "",
+//       })
+//       .expect(200);
+//   });
+// });
--- users/users-router.spec.js
+++ users/users-router.spec.js
@@ -2,24 +2,24 @@ const request = require("supertest");
 const server = require("../server.js");
 
 describe("users-router.js", () => {
-  it("should return a status code of 200", async () => {
-    const response = await request(server).get("/");
-    expect(response.status).toEqual(200);
+  it("should return a status code of 401 without JWT", async () => {
+    const response = await request(server).get("/api/users/");
+    expect(response.status).toEqual(401);
   });
-  it("should return a status of 404 if not logged in for /:id", async () => {
-    const response = await request(server).get("/1");
-    expect(response.status).toEqual(404);
+  it("should return a status of 401 without JWT for /:id", async () => {
+    const response = await request(server).get("/api/users/1");
+    expect(response.status).toEqual(401);
   });
-  it("should return a status of 404 if not logged in for /:id/favorites", async () => {
-    const response = await request(server).get("/1/favorites");
-    expect(response.status).toEqual(404);
+  it("should return a status of 401 without JWT for /:id/favorites", async () => {
+    const response = await request(server).get("/api/users/1/favorites");
+    expect(response.status).toEqual(401);
   });
-  it("should return a status of 404 if not logged in for /:id/favorites/:cityId", async () => {
-    const response = await request(server).get("/1/favorites/1");
-    expect(response.status).toEqual(404);
+  it("should return a status of 401 without JWT for /:id/favorites/:cityId", async () => {
+    const response = await request(server).get("/api/users/1/favorites/1");
+    expect(response.status).toEqual(401);
   });
 
-  it("expects JSON", () => {
-    return request(server).get("/").expect("Content-Type", /html/);
+  it("expects HTML", () => {
+    return request(server).get("/api/users/").expect("Content-Type", /json/);
   });
 });
