1532c1fc5fa6659131b0f1c1d64027e8e44e9e2e
Zac Smith <zrs3141592@gmail.com>
2020-04-29 21:47:31 -0600

    final auth tests

--- routers/authentication-router.spec.js
+++ routers/authentication-router.spec.js
@@ -14,9 +14,9 @@ describe("api test", () => {
 });
 
 describe("auth-router.js", () => {
-  //   beforeEach(async () => {
-  //     await db.raw("truncate users, users restart identity cascade");
-  //   });
+  beforeEach(async () => {
+    await db.raw("truncate users, users restart identity cascade");
+  });
 
   describe("the /api endpoints", async () => {
     it("should return a 404 status code", async () => {
@@ -26,6 +26,29 @@ describe("auth-router.js", () => {
     it("should return a JSON object", async () => {
       const response = await request(server).get("/api/auth");
       expect(response.type).toEqual("text/html");
+    });
+    it("should return an 400 status code with no payload for login", async () => {
+      const response = await request(server).post("/api/auth/signup");
+      expect(response.status).toEqual(400);
+    });
+    it("should return a JSON object from /signup", async () => {
+      const response = await request(server)
+        .post("/api/auth/signup")
+        .send(newUser);
+      expect(response.status).toBe(201);
+      expect(response.type).toEqual("application/json");
+    });
+    it("should return an 400 status code from /signup with no payload for login", async () => {
+      const response = await request(server).post("/api/auth/signin");
+      expect(response.status).toEqual(400);
+    });
+    it("should return a JSON object from /signup", async () => {
+      await request(server).post("/api/auth/signup").send(newUser);
+      const response = await request(server)
+        .post("/api/auth/login")
+        .send(newUser);
+      expect(response.status).toBe(200);
+      expect(response.type).toEqual("application/json");
     });
   });
 });
