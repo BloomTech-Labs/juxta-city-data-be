b3c060c2615edda29df0fbc95ee2b87c0c9fa010
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-06-01 18:34:00 -0700

    Registration cleanup

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,21 +1,21 @@
-const jwt = require("jsonwebtoken");
-const secrets = require("../config/secrets");
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
 
 module.exports = (req, res, next) => {
-    // console.log(req.headers.authorization, "req.headers.authorization.")
-    const token = req.headers.authorization;
-     //const token = req.headers.authorization.split(' ')[1]; // uncomment this line to test the endpoints in postmen 
+  // console.log(req.headers.authorization, "req.headers.authorization.")
+  const token = req.headers.authorization;
+  //const token = req.headers.authorization.split(' ')[1]; // uncomment this line to test the endpoints in postmen
   if (token) {
     //console.log("hello")
     jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
       if (err) {
-        res.status(401).json({ message: "Not authorized to enter" });
+        res.status(401).json({ message: 'Not authorized to enter' });
       } else {
         req.decodedJwt = decodedToken;
         next();
       }
     });
   } else {
-    res.status(401).json({ message: "Not authorized token not present" });
+    res.status(401).json({ message: 'Not authorized token not present' });
   }
 };
